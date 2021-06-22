71ac7def454f34376d0250a2334ca37226c263a7
oleschum <oleschum@users.noreply.github.com>
2021-06-22 11:54:42 +0000

    jekyll build from Action 2114c23cf5cd67f4bc675b7af2fe0f1d144ab148

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "About RadarScenes",
+        "excerpt":"The RadarScenes data set contains data from four automotive radar sensors mounted on one measurement vehicle. The data set was recorded between 2016 and 2018 in Ulm, Germany. Download Several publications from the machine learning field make use of the data set. A pre-print of the RadarScenes paper can be...","categories": [],
+        "tags": [],
+        "url": "/dataset/about/",
+        "teaser": null
+      },{
+        "title": "Examples",
+        "excerpt":"The following galleries contain some example scenes from the data set. Intersection with multiple road users. Inner city scene with pedestrians, cars and a bicycle. Radar transvision effect: the vehicles in front of the ego-vehicle can be seen even though the direct line of sight is blocked. Annotation of a...","categories": [],
+        "tags": [],
+        "url": "/dataset/examples/",
+        "teaser": null
+      },{
+        "title": "Labeling",
+        "excerpt":"General Data annotation for automotive radar point clouds is an extremely tedious task. It requires human experts with a lot of experience to accurately identify all radar points which correspond to the objects of interest. In this data set, only moving objects were annotated. That is, the static environment (trees,...","categories": [],
+        "tags": [],
+        "url": "/dataset/labeling/",
+        "teaser": null
+      },{
+        "title": "Sensor Setup",
+        "excerpt":"The data set was recorded using four series automotive radar sensors. Addtionally, images from a documentary camera are recorded. Ego-motion information of the ego-vehicle is recorded using the car’s odometry sensors as well as a DGPS system. The two side-facing sensors are tilted 85° outwards and the other two sensors...","categories": [],
+        "tags": [],
+        "url": "/dataset/sensors/",
+        "teaser": null
+      },{
+        "title": "Statistics",
+        "excerpt":"The Numbers Four radar sensors One documentary camera &gt; 100 km driving &gt; 4 hours recording time 158 different sequences &gt; 7500 unique road users 11 different object classes About 4 million annotated detections The Figures Feel free to reproduce these figures right from the data set itself! Number of...","categories": [],
+        "tags": [],
+        "url": "/dataset/statistics/",
+        "teaser": null
+      },{
+        "title": "Structure of the Data Set",
+        "excerpt":"General The data set consist of 158 individual sequences. For each sequence, the recorded data from radar and odometry sensors are stored in one hdf5 file. Each of these files is accompanied by a json file called “scenes.json” in which meta-information are stored. In a subfolder, the camera images are...","categories": [],
+        "tags": [],
+        "url": "/dataset/structure/",
+        "teaser": null
+      },{
+        "title": "Tools & API",
+        "excerpt":"Working with radar data is quite different from working with camera or lidar data. To make the start easier, we created some tools to help you. Viewer You can find a viewer for the RadarScenes data set on pypi and on github. To install the viewer, simply follow the instructions...","categories": [],
+        "tags": [],
+        "url": "/dataset/tools/",
+        "teaser": null
+      },{
+        "title": "RadarScenes - Online Now!",
+        "excerpt":"Our data set RadarScenes is now online availble and can be freely used according its license.   The data set can be downloaded here.  ","categories": ["Dataset"],
+        "tags": ["radar","automotive","machine learning"],
+        "url": "/dataset/online-now/",
+        "teaser": null
+      }]
